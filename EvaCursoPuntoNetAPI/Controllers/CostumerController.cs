540cedd7419ea7c9e6ad00685aa191996f5baef5
DavidVillavicencio <david.vs145@gmail.com>
2023-09-24 20:47:54 -0300

    Agregar archivos de proyecto.

---
+++ EvaCursoPuntoNetAPI/Controllers/CostumerController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EvaCursoPuntoNetAPI.Models;
+
+namespace EvaCursoPuntoNetAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CostumerController : ControllerBase
+    {
+        private readonly AdventureWorksLt2019Context _context;
+
+        public CostumerController(AdventureWorksLt2019Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Leer Customer
+        [HttpGet("Listar Customer")]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomer()
+        {
+            if (_context.Customers == null)
+            {
+                return NotFound();
+            }
+            return await _context.Customers.ToListAsync();
+        }
+
+        // GET: api/Customer/5
+        [HttpGet("Buscar por ID")]
+        public async Task<ActionResult<Customer>> GetCustomer(int id)
+        {
+            if (_context.Customers == null)
+            {
+                return NotFound();
+            }
+            var customer = await _context.Customers.FindAsync(id);
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return customer;
+        }
+
+        // PUT: api/Customer/5
+        [HttpPut("Editar")]
+        public async Task<IActionResult> PutCustomer(int id, Customer customer)
+        {
+            if (id != customer.CustomerId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(customer).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CustomerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Customer
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost("Crear")]
+        public async Task<ActionResult<Customer>> PostPerson(Customer customer)
+        {
+            if (_context.Customers == null)
+            {
+                return Problem("Entity set 'AdventureWorksLT2019Context.Customers'  is null.");
+            }
+            _context.Customers.Add(customer);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (CustomerExists(customer.CustomerId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetCustomer", new { id = customer.CustomerId }, customer);
+        }
+
+        // DELETE: api/Customer/5
+        [HttpDelete("Eliminar")]
+        public async Task<IActionResult> DeleteCustomer(int id)
+        {
+            if (_context.Customers == null)
+            {
+                return NotFound();
+            }
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            _context.Customers.Remove(customer);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CustomerExists(int id)
+        {
+            return (_context.Customers?.Any(e => e.CustomerId == id)).GetValueOrDefault();
+        }
+    }
+}
